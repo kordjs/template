52f131738214c5516db56c162b0c566d7b188fc6
sawdev10 <0sapphy@gmail.com>
2025-07-06 23:06:03 +0000

    chore: update Prettier configuration to enforce semicolons and enhance ESLint global ignores

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist/', 'node_modules/']),
+  globalIgnores(['dist/', 'node_modules/', 'tests/.node', 'docs/']),
   {
     files: ['**/*.{ts,mts,cts}'],
     linterOptions: {
@@ -22,5 +22,6 @@ export default defineConfig([
       globals: { ...globals.browser, ...globals.node }
     }
   },
+
   tseslint.configs.strict
-])
+]);
