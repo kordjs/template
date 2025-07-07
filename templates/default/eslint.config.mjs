c878252ab9e223deb45cb679c400f726adde59b9
sawdev10 <0sapphy@gmail.com>
2025-07-07 18:54:09 +0000

    feat: Default template

---
+++ templates/default/eslint.config.mjs
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist/', 'node_modules/', 'tests/.node', 'docs/']),
+  {
+    files: ['**/*.{ts,mts,cts}'],
+    linterOptions: {
+      reportUnusedDisableDirectives: 'warn'
+    }
+  },
+  {
+    files: ['**/*.{ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended']
+  },
+  {
+    files: ['**/*.{ts,mts,cts}'],
+    languageOptions: {
+      globals: { ...globals.browser, ...globals.node }
+    }
+  },
+
+  tseslint.configs.strict
+]);
