c259be8efae2dafa6316a89c366841d3f67b763d
sawdev10 <0sapphy@gmail.com>
2025-07-07 18:47:58 +0000

    chore: rebuild app + formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,24 +4,24 @@ import tseslint from 'typescript-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(['dist/', 'node_modules/', 'tests/.node', 'docs/']),
-  {
-    files: ['**/*.{ts,mts,cts}'],
-    linterOptions: {
-      reportUnusedDisableDirectives: 'warn'
-    }
-  },
-  {
-    files: ['**/*.{ts,mts,cts}'],
-    plugins: { js },
-    extends: ['js/recommended']
-  },
-  {
-    files: ['**/*.{ts,mts,cts}'],
-    languageOptions: {
-      globals: { ...globals.browser, ...globals.node }
-    }
-  },
+    globalIgnores(['dist/', 'node_modules/', 'tests/', 'bin']),
+    {
+        files: ['**/*.{ts,mts,cts}'],
+        linterOptions: {
+            reportUnusedDisableDirectives: 'warn'
+        }
+    },
+    {
+        files: ['**/*.{ts,mts,cts}'],
+        plugins: { js },
+        extends: ['js/recommended']
+    },
+    {
+        files: ['**/*.{ts,mts,cts}'],
+        languageOptions: {
+            globals: { ...globals.browser, ...globals.node }
+        }
+    },
 
-  tseslint.configs.strict
+    tseslint.configs.strict
 ]);
